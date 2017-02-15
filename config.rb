1588560d4228380759c9567ae9ae54130bacb3ca
Kevin Masson <contact.kevinmasson@gmail.com>
2017-02-15 22:24:25 +0000

    Add roboto font

--- config.rb
+++ config.rb
@@ -3,16 +3,18 @@ require 'compass/import-once/activate'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "webroot/css"
-sass_dir = "webroot/sass"
-images_dir = "webroot/img"
-javascripts_dir = "webroot/js"
+project_path = "webroot/"
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "img"
+javascripts_dir = "js"
+fonts_dir = "fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
@@ -22,4 +24,4 @@ javascripts_dir = "webroot/js"
 # project again passing --syntax sass, or you can uncomment this:
 # preferred_syntax = :sass
 # and then run:
-# sass-convert -R --from scss --to sass webroot/sass scss && rm -rf sass && mv scss sass
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
