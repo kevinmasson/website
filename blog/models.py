b614ee0bee93c0400a1673937f2d7203e1721b3b
Kevin Masson <contact.kevinmasson@gmail.com>
2017-05-29 17:16:25 +0200

    Improve article model

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,27 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 class Article(models.Model):
-    title = models.CharField(max_length=80)
-    date = models.DateTimeField()
-    body = models.TextField()
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    text = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return self.title
 
     def __str__(self):
         return self.title
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ('blog_post_detail', (),
+                {
+                    'slug': self.slug,
+                })
+
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super(Article, self).save(*args, **kwargs)
