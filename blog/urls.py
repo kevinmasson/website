fc798202bd096fb24077329cd1051ca045f4ccab
Kevin Masson <contact.kevinmasson@gmail.com>
2017-06-01 16:30:06 +0200

    Add list view for articles

--- blog/urls.py
+++ blog/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import * 
+from django.views.generic.list import ListView
 from blog.models import Article
 from blog.views import *
 
@@ -8,6 +9,14 @@ from blog.views import *
         #url(r'^(?P<code>[A-Za-z0-9]+)$', views.url, name='url'),
 #        ]
 urlpatterns = [
+    url(r'^$', 
+        ListView.as_view(
+            model=Article, 
+            template_name='article_list.html'
+        ),
+        name='article_list'
+        ),
+
     url(r'^(?P<slug>[-\w]+)$', 
         view_article,
         name='blog_article_detail'
