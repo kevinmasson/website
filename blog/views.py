723a219f0136dac9b5e5f158845adb88d4c85321
Kevin Masson <contact.kevinmasson@gmail.com>
2017-07-09 15:47:13 +0200

    Order posts by publish date

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.shortcuts import redirect, render_to_response, get_object_or_404
 from django.views.generic.list import ListView
+from django.utils import timezone
 from .models import Article
 
 class ArticleListView(ListView):
     template_name="article_list.html"
-    queryset = Article.objects.order_by('-created_on')
+    queryset = Article.objects \
+            .filter(publish_date__lt=timezone.now()) \
+            .order_by('-publish_date')
