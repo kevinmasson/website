dadc40ba5f41808d122a6c489dd74ae02e41d50a
Kevin Masson <contact.kevinmasson@gmail.com>
2017-08-10 17:43:30 +0200

    Refactor admin register for the work model

--- post/admin.py
+++ post/admin.py
@@ -1,4 +1,33 @@
 from django.contrib import admin
-from .models import Post
+from django.db import models
+from .models import Post, Status
+from django.forms import widgets
 
-admin.site.register(Post)
+def make_published(modeladmin, request, queryset):
+    queryset.update(status=Status.public)
+
+make_published.short_description = "Mark selected items as published"
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'publish_date', 'status']
+    actions = [make_published]
+    prepopulated_fields = {'slug': ('title', ), }
+    date_hierarchy = 'publish_date'
+    ordering = ['-publish_date', 'title']
+    exclude = ('created_on', 'edited_on')
+
+    fieldsets = (
+            ('General', {
+                'classes': ('wide', 'extrapretty',),
+                'fields': ['title', 'status', 'slug', 'publish_date']
+                }
+            ),
+            ('Content', {
+                'classes': ['collapse',],
+                'fields': ['thumbnail', 'text']
+                }
+            )
+
+            )
+
+admin.site.register(Post, PostAdmin)
